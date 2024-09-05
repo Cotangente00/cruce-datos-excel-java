b35d32a49c6c174fbae38c6045819dd69e44be41
Cotangente00 <julianavila458@gmail.com>
2024-09-05 09:13:13 -0500

    Función para eliminar las primeras 4 filas del archivo probada y funciona correctamente por ahora

--- cruce_datos_excel_java/src/main/java/manipular_INFORME_SOLICITUDES/eliminar_columnas.java
+++
@@ -1,5 +0,0 @@
-package manipular_INFORME_SOLICITUDES;
-
-public class eliminar_columnas {
-    
-}
---
+++ cruce_datos_excel_java/src/main/java/manipular_INFORME_SOLICITUDES/eliminar_filas.java
@@ -0,0 +1,49 @@
+package manipular_INFORME_SOLICITUDES;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class eliminar_filas {
+    public static void main(String[] args) {
+        String excelFilePath = "O:/programa/cruce-datos-excel-java/test-lunes-jueves.xlsx"; // Ruta al archivo
+        String newExcelFilePath = "O:/programa/cruce-datos-excel-java/result.xlsx"; // Ruta al nuevo archivo
+
+        try (FileInputStream fis = new FileInputStream(excelFilePath);
+             Workbook wb = new XSSFWorkbook(fis)) {
+
+            // Selecciona la primera hoja índice cero, o la hoja INFORME SOLICITUDES
+            Sheet ws = wb.getSheetAt(0);
+
+            // Itera, elimina las 4 primeras filas incluyendo las filas que están completamente vacías 
+            for (int i = 0; i < 4; i++) {
+                Row row = ws.getRow(i);
+                if (row != null) {
+                    ws.removeRow(row);
+                }
+            }
+
+            // Una vez eliminadas las filas, se recorre las filas para reajustar el índice 
+            for (int i = 4; i <= ws.getLastRowNum(); i++) {
+                Row row = ws.getRow(i);
+                if (row != null) {
+                    ws.shiftRows(i, i, -4);
+                }
+            }
+
+            // Escribe los cambios en el archivo Excel
+            try (FileOutputStream fos = new FileOutputStream(newExcelFilePath)) {
+                wb.write(fos);
+            }
+
+            System.out.println("Las primeras 4 filas han sido eliminadas con éxito.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
