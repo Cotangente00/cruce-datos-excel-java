c8363df537bc16e1f105e207680ea8a47767fae5
Cotangente00 <julianavila458@gmail.com>
2024-09-05 12:50:59 -0500

    Función para eliminar las columnas innecesarias, funcionando, función para filtrar las ciudades y borrar las ciudades que NO se necesitran, en proceso de desarrollo

--- cruce_datos_excel_java/src/main/java/main/Main.java
+++ cruce_datos_excel_java/src/main/java/main/Main.java
@@ -4,5 +4,6 @@ import manipular_INFORME_SOLICITUDES.*;
 public class Main {
     public static void main(String[] args) {
         eliminar_filas.main(args);
+        eliminar_columnas.main(args);
     }
 }
