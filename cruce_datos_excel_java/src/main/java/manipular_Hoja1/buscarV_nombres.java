f1fad6b8524d81b8a5bc5c90ef827a361c174098
Cotangente00 <julianavila458@gmail.com>
2024-09-20 15:07:51 -0500

    función "simulate_BUSCARV.java" corregida para archivos en formato .xls

--- cruce_datos_excel_java/src/main/java/manipular_Hoja1/buscarV_nombres.java
+++ cruce_datos_excel_java/src/main/java/manipular_Hoja1/buscarV_nombres.java
@@ -1,6 +1,12 @@
 package manipular_Hoja1;
 
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.*;
 
 
@@ -30,11 +36,28 @@ public class buscarV_nombres {
 
         while (rowIterator2.hasNext()) {
             Row row = rowIterator2.next();
-            Cell cell = row.getCell(9);
+            Cell cellNumero = row.getCell(9);
+            Cell cellNombre = row.getCell(10);
+
             DataFormatter formatter = new DataFormatter();
-            String numeroBuscar = formatter.formatCellValue(cell);
-            String nombre = row.getCell(10).getStringCellValue();
-            datosINFORME_SOLICITUDES.add(Map.of(numeroBuscar, nombre));
+            String numeroBuscar = formatter.formatCellValue(cellNumero);
+            //Verificar si la celda del nombre no es nula 
+            String nombre = "";
+            if (cellNombre != null) {
+                switch (cellNombre.getCellType()) {
+                    case STRING:
+                        nombre = cellNombre.getStringCellValue();
+                        break;
+                    case NUMERIC:
+                        nombre = String.valueOf(cellNombre.getNumericCellValue());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (!numeroBuscar.isEmpty() && !nombre.isEmpty()) {
+                datosINFORME_SOLICITUDES.add(Map.of(numeroBuscar, nombre));
+            }
         }
 
         // Iterar sobre los números de la Hoja INFORME SOLICITUDES y buscar coincidencias
@@ -54,13 +77,20 @@ public class buscarV_nombres {
         System.out.println("Nombres completos agregados exitosamente.");
     }
 
-    /*public static void main(String[] args) {
-        String rutaArchivo = "O:/programa/cruce-datos-excel-java/result.xlsx";
-        //String rutaArchivoSalida = "O:/programa/cruce-datos-excel-java/result2.xlsx";
+    public static void main(String[] args) throws EncryptedDocumentException, IOException {
+        String inputFilePath = "O:/aa/result.xlsx"; // Ruta del archivo .xls original
+        String outputFilePath = "O:/aa/result2.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(new File(inputFilePath));
+        Workbook wb = WorkbookFactory.create(fileInputStream);
         try {
-            simulateBUSCARVHoja1(rutaArchivo);
+            simulateBUSCARVHoja1(wb);
+            //Guardar archivo 
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(outputFilePath));
+            wb.write(fileOutputStream);
+            fileOutputStream.close();
+            wb.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }*/
+    }
 }
